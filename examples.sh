5485e7c20c729e16c55a9956b29de53762e42794
Skyy Moore <124527392+mooreApps22@users.noreply.github.com>
2025-12-12 07:24:30 -0800

    Add examples for shot_caller.sh usage

---
+++ examples.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo ================ Example 1 ========================
+./shot_caller.sh 5 5 1 'sdxddssaaww'
+echo ================ Example 2 ========================
+./shot_caller.sh 10 6 1 'sdxssdswdxddddsxaadwxwdxwaa'
+echo ================ Example 3 ========================
+./shot_caller.sh 3 3 1 'dxss'
+echo ================ Example 4 ========================
+./shot_caller.sh 3 3 2 'dxss'
+echo ================ Example 5 ========================
+./shot_caller.sh 3 3 3 'dxss'
